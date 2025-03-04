57ef2b10ba534bba382fc966830d548d77f9337e
azilamo <ddoossyyu@gmail.com>
2025-03-04 21:40:35 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+    import { OGP } from "@jill64/svelte-suite";
+
+</script>
+<!-- src/+layout.svelte -->
+<OGP
+  title="OGP テスト"
+  site_name="svelte-suiteを使ったOGPのてすとです"
+  description="jillさんすげぇ"
+  image="https://media.discordapp.net/attachments/1343264919847436448/1346462061718147122/OGP.png"
+/>
