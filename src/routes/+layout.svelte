d27a8d152ef5999344407cbcefd4be4f00b37af3
azilamo <ddoossyyu@gmail.com>
2025-03-10 17:58:52 +0900

    Fix: About Text
    いぇーいお母さんみってるぅ？

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script>
     import "../app.pcss";
     import { OGP } from "@jill64/svelte-suite";
+    import { FlipButton } from "@jill64/svelte-suite";
+    import { ThemeManager } from "@jill64/svelte-suite";
     let { children } = $props();
 </script>
-
 <OGP 
-    title="あじらものサイト"
-    description="あじらもが作ったサイト"
-    site_name="あじらものサイト"
+    title="AzilamoOffical"
+    description="AzilamoOffical's website"
+    site_name="AzilamoOffical"
     image=""
 />
+<ThemeManager />
+<FlipButton />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,11 @@
     const discord = "https://discord.com/users/1130793656635371552"
 </script>
 
-<h1 class="font-mono font-bold text-white text-6xl flex justify-center mt-4">Azilamo</h1>
-<p class="text-white flex justify-center">oddball, beginner programmer | age:13 | 🤔</p>
+
+<h1 class="font-mono font-bold text-black dark:text-white text-6xl flex justify-center mt-4">Azilamo</h1>
+<p class="text-black dark:text-white flex justify-center">weirdo, beginner programmer, Server | age:13 | 🤔</p>
 <div class="flex justify-center gap-2">
     <a class="text-gray-400" href={github}>Github</a>
-    <a class="text-white" href={twitter}>𝕏</a>
+    <a class="text-black dark:text-white" href={twitter}>𝕏</a>
     <a class="text-purple-500" href={discord}>Discord</a>
 </div>
