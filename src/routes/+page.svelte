2d6c13355f2724af9b3b10031ed9bbe1c7b620e6
azilamo <ddoossyyu@gmail.com>
2025-03-13 15:58:02 +0900

    Fix: なぞのタグを消去

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,4 +16,3 @@
     <a class="text-blue-500" href={bluesky}>Bluesky</a>
     <a class="text-blue-300" href={zenn}>Zenn</a>
 </div>
-<p class="text-black dark:text-white flex justify-end mt-7">(im amanens)</p>
