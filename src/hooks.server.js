e8c53fec729bc750047f053f275ba272c2362c39
azilamo <ddoossyyu@gmail.com>
2025-03-13 16:06:20 +0900

    Fix:なんちゃらかんちゃら
    急いで書いた

---
+++ src/hooks.server.js
@@ -0,0 +1,9 @@
+// hooks.server.js
+import { onRender } from '@jill64/svelte-dark-theme'
+
+export const handle = onRender({
+  // Optional Configs
+  // {
+  //   cookieKey: string (default: 'svelte-dark-theme')
+  // }
+})
