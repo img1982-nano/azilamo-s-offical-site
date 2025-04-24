e0a4d32b03aa1923fc642dc07929afed5d38c3fb
img1982-nano <ddoossyyu+img@gmail.com>
2025-04-24 10:16:22 +0000

    Add:Project

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -14,4 +14,21 @@
         alt="Readme Card by GitHub Readme Stats"
         loading="lazy">
     </picture>
-  </a>
+</a>
+<h1 class="font-mono font-bold text-black dark:text-white text-6xl flex justify-start mt-4">Quiz</h1>
+<img src="https://ul.h3z.jp/WCVILENe.gif">
+<p class="font-mono font-bold text-black dark:text-white">謎のクイズです</p>
+<a class="font-mono font-bold text-yellow-400 dark:text-yellow-400" href="https://quiz.azilamo.com">https://quiz.azilamo.com</a>
+<a href="https://github.com/img1982-nano/quiz" target="_blank" rel="noopener">
+  <picture>
+    <!-- 生成されるグラフは SVG なので type="image/svg+xml" -->
+    <source 
+      srcset="https://github-readme-stats.vercel.app/api/pin/?username=img1982-nano&repo=quiz&theme=dark&hide_border=true" 
+      type="image/svg+xml">
+    <!-- フォールバック画像 -->
+    <img 
+      src="https://github-readme-stats.vercel.app/api/pin/?username=img1982-nano&repo=quiz&theme=dark&hide_border=true" 
+      alt="Readme Card by GitHub Readme Stats"
+      loading="lazy">
+  </picture>
+</a>
