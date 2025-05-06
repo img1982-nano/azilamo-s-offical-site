18d382c52c5f0896844a9912480d02d3a359fd10
img1982-nano <ddoossyyu+img@gmail.com>
2025-05-06 17:12:44 +0000

    Fix: Same

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltePreprocess } from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.svx'],
   preprocess: [
-    sveltePreprocess({
-      scss: { includePaths: ['./src/css'] },
-      postcss: true
-    }),
+    sveltePreprocess({ scss: { includePaths: ['./src/css'] }, postcss: true }),
     mdsvex({ extension: '.svx' })
   ],
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    })
+    adapter: adapter()
   }
 };
 
